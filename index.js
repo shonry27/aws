8b03490320a295da8c7ae3b5ccad492f9f950293
Shawn dsouza <shawn62billie@gmail.com>
2018-01-16 11:38:37 +0530

    index.js
    This is a nodejs 6 function in AWS Lambda to create entries in dynamo db

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log('starting function');
+const AWS= require('aws-sdk');
+const docClient=new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+
+exports.handle = function(e, ctx, cb) 
+{
+
+	var params={
+		Item: {
+			date: Date.now(),
+			message: "This Website Rocks! "
+		},
+		TableName: 'guestbook'
+	};
+	docClient.put(params,function(err, data){
+		if(err){
+			cb(err,null);
+			
+		}else{
+			cb(null,data);
+		}
+	});
+}
